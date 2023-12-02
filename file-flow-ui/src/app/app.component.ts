9376cd8a72ddc2db30dca16cf72155a0da7f97aa
Bill Yang <billyang3416@gmail.com>
2023-12-02 15:59:38 +0800

    implement app layout

--- file-flow-ui/src/app/app.component.ts
+++ file-flow-ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/enviroments/environment.dev';
+import { ApiPrefix, ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'file-flow-ui';
+
+  lineLoginUrl = environment.apiUrl + '/auth/line-login';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {}
+
+  logout() {
+    this.apiService.get(ApiPrefix.AUTH, 'logout', {}).subscribe();
+  }
 }
--- file-flow-ui/src/app/app.module.ts
+++ file-flow-ui/src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
