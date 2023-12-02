262bbdfff59a81d8e6c7aa0ed5cb0c4d719a3e75
Bill Yang <billyang3416@gmail.com>
2023-12-02 17:35:06 +0800

    add upload file btn and email input

--- file-flow-ui/src/app/features/file-flow/file-flow.component.ts
+++ file-flow-ui/src/app/features/file-flow/file-flow.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-file-flow',
   templateUrl: './file-flow.component.html',
-  styleUrls: ['./file-flow.component.scss']
+  styleUrls: ['./file-flow.component.scss'],
 })
 export class FileFlowComponent {
+  fileName = '';
 
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+    }
+  }
 }
--- file-flow-ui/src/app/features/file-flow/file-flow.module.ts
+++ file-flow-ui/src/app/features/file-flow/file-flow.module.ts
@@ -3,8 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FileFlowComponent } from './file-flow.component';
 import { FileFlowRoutingModule } from './file-flow-routing.module';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [FileFlowComponent],
-  imports: [CommonModule, FileFlowRoutingModule],
+  imports: [
+    CommonModule,
+    FileFlowRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class FileFlowModule {}
