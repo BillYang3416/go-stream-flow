0e25985b143ac5e2e4eb8df8052031ede80cfd1a
Bill Yang <billyang3416@gmail.com>
2023-12-02 16:11:36 +0800

    add interface for usecase

---
+++ go-file-gate/internal/usecase/interfaces.go
@@ -0,0 +1,19 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/bgg/go-file-gate/internal/entity"
+)
+
+type UserProfile interface {
+	Create(context.Context, entity.UserProfile) (entity.UserProfile, error)
+	GetByID(context.Context, string) (entity.UserProfile, error)
+	UpdateRefreshToken(context.Context, string, string) error
+}
+
+type UserProfileRepo interface {
+	Create(context.Context, entity.UserProfile) error
+	GetByID(context.Context, string) (entity.UserProfile, error)
+	UpdateRefreshToken(context.Context, string, string) error
+}
