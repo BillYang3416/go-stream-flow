b47cb4280e26f4adb399904b5c3a44d2cd2fd24a
Bill Yang <billyang3416@gmail.com>
2023-12-03 16:52:42 +0800

    add unit test of user uploaded file repo

---
+++ go-file-gate/internal/usecase/repo/user_uploaded_file_postgres_test.go
@@ -0,0 +1,72 @@
+package repo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/bgg/go-file-gate/internal/entity"
+	"github.com/bgg/go-file-gate/pkg/postgres"
+	"github.com/pashagolub/pgxmock/v3"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupUserUploadedFileRepoTest(t *testing.T) (context.Context, pgxmock.PgxPoolIface, *UserUploadedFileRepo) {
+
+	ctx := context.Background()
+	mock, err := pgxmock.NewPool()
+	assert.NoError(t, err, "Error should not have occurred when opening a stub database connection")
+	defer mock.Close()
+
+	pg := &postgres.Postgres{Pool: mock, Builder: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)}
+	repo := NewUserUploadedFileRepo(pg)
+	return ctx, mock, repo
+}
+
+func TestUserUploadedFile_Created(t *testing.T) {
+
+	t.Run("should create a user uploaded file", func(t *testing.T) {
+
+		// Arrange
+		ctx, mock, repo := setupUserUploadedFileRepoTest(t)
+
+		userUploadedFile := entity.UserUploadedFile{
+			Name:      "test.txt",
+			Size:      123,
+			Content:   []byte("test"),
+			UserID:    "123",
+			EmailSent: false,
+		}
+
+		mock.ExpectExec("INSERT INTO user_uploaded_files").
+			WithArgs(userUploadedFile.Name, userUploadedFile.Size, userUploadedFile.Content, userUploadedFile.UserID, userUploadedFile.EmailSent).
+			WillReturnResult(pgxmock.NewResult("INSERT", 1))
+
+		// Act
+		err := repo.Create(ctx, userUploadedFile)
+
+		// Assert
+		assert.NoError(t, err, "Error should not have occurred when creating a user uploaded file")
+		mock.ExpectationsWereMet()
+	})
+
+	t.Run("should return an error when creating a user uploaded file", func(t *testing.T) {
+
+		// Arrange
+		ctx, mock, repo := setupUserUploadedFileRepoTest(t)
+
+		userUploadedFile := entity.UserUploadedFile{}
+
+		mock.ExpectExec("INSERT INTO user_uploaded_files").
+			WithArgs(userUploadedFile.Name, userUploadedFile.Size, userUploadedFile.Content, userUploadedFile.UserID, userUploadedFile.EmailSent).
+			WillReturnError(assert.AnError)
+
+		// Act
+		err := repo.Create(ctx, userUploadedFile)
+
+		// Assert
+		assert.Error(t, err, "Error should have occurred when creating a user uploaded file")
+		mock.ExpectationsWereMet()
+	})
+
+}
