046ff10a1bf3f17bbaa10dc76362c69ad5afe9b0
Bill Yang <billyang3416@gmail.com>
2023-12-08 12:57:24 +0800

    change domail url

--- file-flow-ui/src/environments/environment.dev.ts
+++ file-flow-ui/src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/api/v1',
+  apiUrl:
+    'https://69fb-2001-b011-380f-192b-c8e4-df40-3a48-c977.ngrok-free.app/api/v1',
 };
