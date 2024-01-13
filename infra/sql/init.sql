587fa24b28b66894a3833d1df9d7d62439af8257
Bill Yang <billyang3416@gmail.com>
2024-01-13 21:08:41 +0800

    add oauth_detail and credential schema

--- infra/sql/init.sql
+++ infra/sql/init.sql
@@ -1,10 +1,25 @@
 -- User Profile
 CREATE TABLE user_profiles (
-    user_id VARCHAR(255) PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     display_name VARCHAR(255) NOT NULL,
-    picture_url VARCHAR(255),
+    picture_url VARCHAR(255)
+);
+
+-- Oauth Details
+CREATE TABLE oauth_details (
+    oauth_id VARCHAR(255) PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES user_profiles(user_id),
+    provider VARCHAR(255) NOT NULL,
     access_token VARCHAR(255) NOT NULL,
     refresh_token VARCHAR(255) NOT NULL
+);
+
+-- Credentials
+CREATE TABLE user_credentials (
+    credential_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES user_profiles(user_id),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
 
 -- User File
@@ -13,7 +28,7 @@ CREATE TABLE user_uploaded_files (
     name VARCHAR(255) NOT NULL,
     size BIGINT NOT NULL,
     content BYTEA NOT NULL,
-    user_id VARCHAR(255) NOT NULL REFERENCES user_profiles(user_id),
+    user_id int NOT NULL REFERENCES user_profiles(user_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
     email_sent BOOLEAN NOT NULL,
     email_sent_at TIMESTAMPTZ,
