7b1052da32b2e825ed589150bd58c4691652f512
Bill Yang <billyang3416@gmail.com>
2024-01-15 22:01:53 +0800

    setup frontend call api for sign-in and sign-up

--- file-flow-ui/src/app/features/login/login.component.ts
+++ file-flow-ui/src/app/features/login/login.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ApiService } from 'src/app/core/services/api.service';
+import { MatTabGroup } from '@angular/material/tabs';
+import { Router } from '@angular/router';
+import { ApiPrefix, ApiService } from 'src/app/core/services/api.service';
 import { environment } from 'src/environments/environment.dev';
 
 @Component({
@@ -9,7 +12,9 @@ import { environment } from 'src/environments/environment.dev';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
+
   signInForm!: FormGroup;
 
   signUpForm!: FormGroup;
@@ -19,7 +24,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private apiSvc: ApiService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -30,8 +36,14 @@ export class LoginComponent implements OnInit {
 
     this.signUpForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
-      nickname: ['', [Validators.required]],
+      displayName: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.tabGroup.selectedTabChange.subscribe((_) => {
+      this.onReset();
     });
   }
 
@@ -40,15 +52,48 @@ export class LoginComponent implements OnInit {
       this.snackBar.open('Please enter username and password correctly', 'OK');
       return;
     }
+    const value = this.signInForm.value;
+    this.apiSvc.post(ApiPrefix.AUTH, 'login', value).subscribe({
+      next: (_) => {
+        this.onReset();
+        this.snackBar.open('Sign in successfully!', '', {
+          duration: 2000,
+        });
+        this.router.navigate(['/file-flow']);
+      },
+      error: (err: HttpErrorResponse) => {
+        this.snackBar.open(err.error.message, '', { duration: 2000 });
+      },
+    });
   }
 
   onSignUp(): void {
-    if (this.signInForm.invalid) {
+    if (this.signUpForm.invalid) {
       this.snackBar.open(
         'Please enter username,password and nickname correctly',
         'OK'
       );
       return;
     }
+    const value = this.signUpForm.value;
+    this.apiSvc.post(ApiPrefix.AUTH, 'register', value).subscribe({
+      next: (_) => {
+        this.onReset();
+        this.snackBar.open('Sign up successfully, please try to sign in.', '', {
+          duration: 2000,
+        });
+        this.tabGroup.selectedIndex = 0;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.snackBar.open(err.error.message, '', { duration: 2000 });
+      },
+    });
+  }
+
+  onReset() {
+    this.signInForm.reset();
+    this.signInForm.setErrors(null);
+    this.signUpForm.reset();
+    this.signUpForm.setErrors(null);
   }
 }
