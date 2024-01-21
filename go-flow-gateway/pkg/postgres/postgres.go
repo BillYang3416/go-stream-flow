fcc49b34fdc1cd20d621bf8a4c0fadbf6c06eb2f
Bill Yang <billyang3416@gmail.com>
2024-01-21 18:52:54 +0800

    add query method on PgxPoolIface

--- go-flow-gateway/pkg/postgres/postgres.go
+++ go-flow-gateway/pkg/postgres/postgres.go
@@ -22,6 +22,7 @@ const (
 
 type PgxPoolIface interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 	Close()
 }
