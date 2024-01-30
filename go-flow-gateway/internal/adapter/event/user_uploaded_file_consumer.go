1b3205db0694ce270dc63ecd8e57a83c69ccb038
Bill Yang <billyang3416@gmail.com>
2024-01-30 21:56:13 +0800

    refactor consumer with error handling and logging

--- go-flow-gateway/internal/adapter/event/user_uploaded_file_consumer.go
+++ go-flow-gateway/internal/adapter/event/user_uploaded_file_consumer.go
@@ -12,14 +12,14 @@ import (
 )
 
 type UserUploadedFileConsumer struct {
-	u  usecase.UserUploadedFile
-	l  logger.Logger
-	ch *amqp.Channel
-	q  amqp.Queue
+	userUploadedFile usecase.UserUploadedFile
+	logger           logger.Logger
+	ch               *amqp.Channel
+	queue            amqp.Queue
 }
 
 func NewUserUploadedFileConsumer(u usecase.UserUploadedFile, ch *amqp.Channel, l logger.Logger) *UserUploadedFileConsumer {
-	cs := &UserUploadedFileConsumer{u: u, ch: ch, l: l}
+	cs := &UserUploadedFileConsumer{userUploadedFile: u, ch: ch, logger: l}
 
 	q, err := ch.QueueDeclare(
 		"user-uploaded-file-created-queue",
@@ -30,48 +30,53 @@ func NewUserUploadedFileConsumer(u usecase.UserUploadedFile, ch *amqp.Channel, l
 		nil,   // arguments
 	)
 	if err != nil {
-		cs.l.Error(err, "failed to declare queue")
+		cs.logger.Error(err, "failed to declare queue")
 	}
-	cs.q = q
+	cs.queue = q
 	return cs
 }
 
 func (cs *UserUploadedFileConsumer) StartConsume() {
-	cs.l.Info(" UserUploadedFileConsumer - StartConsume - Waiting for messages.")
+	cs.logger.Info("UserUploadedFileConsumer - StartConsume: start consuming messages")
 
 	msgs, err := cs.ch.Consume(
-		cs.q.Name, // queue
-		"",        // consumer
-		true,      // auto-ack
-		false,     // exclusive
-		false,     // no-local
-		false,     // no-wait
-		nil,       // args
+		cs.queue.Name, // queue
+		"",            // consumer
+		true,          // auto-ack
+		false,         // exclusive
+		false,         // no-local
+		false,         // no-wait
+		nil,           // args
 	)
 	if err != nil {
-		cs.l.Error(err, "failed to register a consumer")
+		cs.logger.Error("UserUploadedFileConsumer - StartConsume - ch.Consume: failed to register a consumer", "error", err)
 	}
+	cs.logger.Info("UserUploadedFileConsumer - StartConsume: successfully registered a consumer")
 
 	for d := range msgs {
-
-		var userUploadedFile entity.UserUploadedFile
-		err := json.Unmarshal(d.Body, &userUploadedFile)
-		if err != nil {
-			cs.l.Error(err, "failed to unmarshal message")
-		}
-		cs.l.Info("Received a message: %s", userUploadedFile.Name)
-
-		decodedContent, err := base64.StdEncoding.DecodeString(userUploadedFile.Base64Content)
-		if err != nil {
-			cs.l.Error(err, "failed to decode base64 content")
-		}
-		userUploadedFile.Content = decodedContent
-
-		err = cs.u.SendEmail(context.Background(), userUploadedFile)
-		if err != nil {
-			cs.l.Error(err, "failed to send email")
-			continue
-		}
+		cs.processMessage(d)
 	}
 
+}
+
+func (cs *UserUploadedFileConsumer) processMessage(d amqp.Delivery) {
+	var userUploadedFile entity.UserUploadedFile
+	err := json.Unmarshal(d.Body, &userUploadedFile)
+	if err != nil {
+		cs.logger.Error("UserUploadedFileConsumer - processMessage - json.Unmarshal: failed to unmarshal message body", "error", err)
+	}
+	cs.logger.Info("UserUploadedFileConsumer - processMessage: successfully unmarshalled message body", "userUploadedFileID", userUploadedFile.ID)
+
+	decodedContent, err := base64.StdEncoding.DecodeString(userUploadedFile.Base64Content)
+	if err != nil {
+		cs.logger.Error("UserUploadedFileConsumer - processMessage - base64.StdEncoding.DecodeString: failed to decode base64 content", "error", err)
+	}
+	userUploadedFile.Content = decodedContent
+	cs.logger.Info("UserUploadedFileConsumer - processMessage: successfully decoded base64 content", "userUploadedFileID", userUploadedFile.ID)
+
+	err = cs.userUploadedFile.SendEmail(context.Background(), userUploadedFile)
+	if err != nil {
+		cs.logger.Error("UserUploadedFileConsumer - processMessage - userUploadedFile.SendEmail: failed to send email", "error", err)
+	}
+	cs.logger.Info("UserUploadedFileConsumer - processMessage: successfully sent email", "userUploadedFileID", userUploadedFile.ID)
 }
